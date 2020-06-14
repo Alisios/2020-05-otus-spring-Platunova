7dde48ef757c280a2319e8035ae1580285b78b5a
Alisios <lavii@ya.ru>
2020-06-14 21:10:16 +0300

    Поправила тесты

--- hw01-xml-configuration/src/test/java/ru/otus/spring/dao/QuestionDaoImplTest.java
+++ hw01-xml-configuration/src/test/java/ru/otus/spring/dao/QuestionDaoImplTest.java
@@ -3,27 +3,31 @@ package ru.otus.spring.dao;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.spring.domain.Question;
 import ru.otus.spring.helper.Parser;
 import ru.otus.spring.helper.ParserCsv;
 
 import java.io.IOException;
 import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 class QuestionDaoImplTest {
-
-    private ClassPathXmlApplicationContext context;
+    private final Parser  parser = mock(Parser.class);
+    private final QuestionDao dao = new QuestionDaoImpl(parser);
 
     @Test
     @DisplayName("корректную работы основных методов дао ")
-    void correctWorkOfDao() throws QuestionDaoException, IOException {
-        URL path = Thread.currentThread().getContextClassLoader().getResource("questions-and-answers.csv");// "";
-        Parser parser = new ParserCsv(Objects.requireNonNull(path).openStream());
-        QuestionDao dao = new QuestionDaoImpl(parser);
+    void correctWorkOfDao() throws IOException {
+        List<Question> list = List.of(new Question(123, "Что происходит?", Collections.emptyList()));
+        when(parser.parseQuestions()).thenReturn(list);
         assertDoesNotThrow(()->{
             assertThat(dao.findAll()).isNotEmpty();
         });
@@ -31,24 +35,10 @@ class QuestionDaoImplTest {
 
     @Test
     @DisplayName("корректную работы основных методов дао при ошибках парсинга")
-    void correctWorkOfDaoWhenParsingRuntimeException() throws QuestionDaoException, IOException {
-        URL path = Thread.currentThread().getContextClassLoader().getResource("questions-and-answersError.csv");// "";
-        assertThrows(QuestionDaoException.class, ()->{
-            Parser parser = new ParserCsv(Objects.requireNonNull(path).openStream());
-            QuestionDao dao = new QuestionDaoImpl(parser);
-            dao.findAll();
-        });
+    void correctWorkOfDaoWhenParsingRuntimeException() throws IOException {
+        doThrow(new IOException()).when(parser).parseQuestions();
+        assertThrows(QuestionDaoException.class, dao::findAll);
     }
 
-    @Test
-    @DisplayName("корректную работы основных методов дао при отсутсвии файла")
-    void correctWorkOfDaoWhenNullPointException() throws QuestionDaoException, IOException {
-        URL path = Thread.currentThread().getContextClassLoader().getResource("questions-and-answersError5674.csv");// "";
-        assertThrows(NullPointerException.class, ()->{
-            Parser parser = new ParserCsv(Objects.requireNonNull(path).openStream());
-            QuestionDao dao = new QuestionDaoImpl(parser);
-            dao.findAll();
-        });
-    }
 
 }
