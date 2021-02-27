5d174af55b7ec1247c8d4d362e6a9b0f48a2f3f5
Alisios <lavii@ya.ru>
2021-02-27 20:37:46 +0300

    statistics

--- investor-api/notifier-service/src/main/java/ru/otus/spring/configuration/TelegramConfig.java
+++ investor-api/notifier-service/src/main/java/ru/otus/spring/configuration/TelegramConfig.java
@@ -9,7 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.map.repository.config.EnableMapRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 import org.telegram.telegrambots.ApiContextInitializer;
+import ru.otus.spring.statisticsservice.StatElement;
+import ru.otus.spring.statisticsservice.StatRepository;
 import ru.otus.spring.telegramservice.Bot;
+
+import java.time.LocalDateTime;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,11 +25,18 @@ public class TelegramConfig {
 
     BotProperties properties;
 
+    StatRepository statRepository;
+
     @Bean
     Bot ticketBot() {
         ApiContextInitializer.init();
         Bot invest_bot = new Bot(properties);
         invest_bot.botConnect();
         return invest_bot;
+    }
+
+    @Bean
+    void initiateDb() {
+        statRepository.save(StatElement.builder().id("statistics").mailCount(0).dateTime(LocalDateTime.now()).telegramCount(0).build());
     }
 }
--- investor-api/notifier-service/src/main/java/ru/otus/spring/messagetransfer/TransferServiceEmail.java
+++ investor-api/notifier-service/src/main/java/ru/otus/spring/messagetransfer/TransferServiceEmail.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.retry.annotation.Backoff;
@@ -12,6 +13,11 @@ import org.springframework.stereotype.Service;
 
 import ru.otus.spring.configuration.MailProperties;
 import ru.otus.spring.kafka.StockInfoForUser;
+import ru.otus.spring.statisticsservice.StatElement;
+import ru.otus.spring.statisticsservice.StatRepository;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -21,11 +27,15 @@ import ru.otus.spring.kafka.StockInfoForUser;
 @Service("email_service")
 @RequiredArgsConstructor
 @Slf4j
-@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class TransferServiceEmail implements TransferService {
 
-    JavaMailSender emailSender;
-    MailProperties mailProperties;
+    private final JavaMailSender emailSender;
+    private final MailProperties mailProperties;
+    private final StatRepository statRepository;
+    
+    @Value("${mail-properties.key}")
+    private static String KEY;
+    private final AtomicInteger count = new AtomicInteger(0);
 
     @Override
     @Retryable(value = Exception.class, maxAttempts = 3, backoff = @Backoff(delay = 500))
@@ -37,6 +47,8 @@ public class TransferServiceEmail implements TransferService {
             message.setSubject("Инвестиции апи");
             message.setText(info.getMessageForUser());
             emailSender.send(message);
+            count.incrementAndGet();
+            statRepository.findById(KEY).get().setMailCount(count.get());
         } catch (Exception ex) {
             log.error("Ошибка отправки сообщения пользователю по почте", ex);
             throw new TransferException("Ошибка отправки сообщения пользователю по почте", ex);
