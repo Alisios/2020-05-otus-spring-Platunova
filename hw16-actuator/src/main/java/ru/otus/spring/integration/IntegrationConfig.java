9a19b50bb40a7837c85dcf2f10d71710facb707a
Alisios <lavii@ya.ru>
2021-02-28 14:05:43 +0300

    добавлено readme

--- hw16-actuator/src/main/java/ru/otus/spring/integration/IntegrationConfig.java
+++ hw16-actuator/src/main/java/ru/otus/spring/integration/IntegrationConfig.java
@@ -11,14 +11,14 @@ import org.springframework.integration.dsl.MessageChannels;
 import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.scheduling.PollerMetadata;
 
-import java.time.LocalDateTime;
-
 @Configuration
 @IntegrationComponentScan
 @Slf4j
 public class IntegrationConfig {
 
     private static final int DEFAULT_QUEUE_CAPACITY = 100;
+    private static final int DEFAULT_QUEUE_PERIOD = 100;
+    private static final int MAX_MESSAGE_PER_POLL = 2;
 
     private static final String UPDATE_CACHE = "updateInfo";
 
@@ -35,16 +35,13 @@ public class IntegrationConfig {
 
     @Bean (name = PollerMetadata.DEFAULT_POLLER )
     public PollerMetadata poller () {
-        return Pollers.fixedRate(100).maxMessagesPerPoll(2).get() ;
+        return Pollers.fixedRate(DEFAULT_QUEUE_PERIOD).maxMessagesPerPoll(MAX_MESSAGE_PER_POLL).get() ;
     }
 
 
     @Bean
     public IntegrationFlow cacheFlow() {
         return f -> f.channel(cacheInfoChannel())
-//                .handle((m)->{
-//                        log.info("Получено сообщение из кафки для обновления кэша: {}", LocalDateTime.now()))
-//                })
                 .handle("cacheService", UPDATE_CACHE);
     }
 
